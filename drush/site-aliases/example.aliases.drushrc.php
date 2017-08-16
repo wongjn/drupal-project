ed18da8a218ef83c20ae690ff9921409e3b69a30
wongjn <jwong25914@msn.com>
2017-08-16 11:08:28 +0100

    Add example drush alias file

---
+++ drush/site-aliases/example.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+$options['root'] = '/home/USER/public_html';
+$options['uri'] = 'http://';
+$options['remote-host'] = 'server.projectcosmic.co.uk';
+$options['remote-user'] = 'REMOTE-USER';
+$options['path-aliases'] = [
+  '%drush-script' => '/home/USER/vendor/bin/drush',
+];
