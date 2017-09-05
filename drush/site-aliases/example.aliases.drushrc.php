e100530df238ede593cd8b76d470d0b0f72ebd6c
wongjn <jwong25914@msn.com>
2017-09-05 14:09:19 +0100

    Remove example alias file

--- drush/site-aliases/example.aliases.drushrc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$options['root'] = '/home/USER/public_html';
-$options['uri'] = 'http://';
-$options['remote-host'] = 'server.projectcosmic.co.uk';
-$options['remote-user'] = 'REMOTE-USER';
-$options['path-aliases'] = [
-  '%drush-script' => '/home/USER/vendor/bin/drush',
-];
