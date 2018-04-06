5e267093f31e3db0c4c48dde76173eceb0139448
wongjn <jwong25914@msn.com>
2018-04-06 13:47:07 +0100

    Remove local drush clear

--- boilerplate/scripts/deploy.sh
+++ boilerplate/scripts/deploy.sh
@@ -6,6 +6,3 @@ git push live master
 # Copy compiled files to server
 theme="public_html/themes/custom/{{ NAME }}"
 scp -r "$theme/dist" "REMOTE_USER@server_.projectcosmic.co.uk:/home/REMOTE_USER/$theme"
-
-# Clear cache (choose)
-drush cc
