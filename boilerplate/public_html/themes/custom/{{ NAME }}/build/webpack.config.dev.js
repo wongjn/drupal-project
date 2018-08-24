95240d938cd672fe4039858cf7b8829859b5bda0
wongjn <jwong25914@msn.com>
2018-08-24 10:58:03 +0100

    Remove remnants of preact development

--- boilerplate/public_html/themes/custom/{{ NAME }}/build/webpack.config.dev.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/build/webpack.config.dev.js
@@ -7,12 +7,6 @@ const baseConfig = require('./webpack.config.base');
 const merge = require('webpack-merge');
 
 module.exports = merge(baseConfig, {
-  entry: ['preact/debug'],
   devtool: 'eval-source-map',
   mode: 'development',
-  resolve: {
-    alias: {
-      preact$: 'preact/dist/preact.js',
-    },
-  },
 });
