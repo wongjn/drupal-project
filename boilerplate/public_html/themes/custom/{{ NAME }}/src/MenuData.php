52fceb2643a846ecca079af5be267b7ab018698c
wongjn <11310624+wongjn@users.noreply.github.com>
2018-10-11 16:07:34 +0100

    Add external link attributes to menu items

--- boilerplate/public_html/themes/custom/{{ NAME }}/src/MenuData.php
+++ boilerplate/public_html/themes/custom/{{ NAME }}/src/MenuData.php
@@ -84,4 +84,29 @@ class MenuData {
     return $info;
   }
 
+  /**
+   * Recursively adds external link related attributes to a menu tree.
+   *
+   * @param array $items
+   *   List of menu items from template_preprocess_menu() or similar.
+   */
+  public static function addExternalOptions(array $items) {
+    foreach ($items as $item) {
+      if ($item['below']) {
+        self::addExternalOptions($item['below']);
+      }
+
+      if (!$item['url']->isExternal()) {
+        continue;
+      }
+
+      $item['url']->mergeOptions([
+        'attributes' => [
+          'target' => '_blank',
+          'rel' => ['nofollow', 'noopener'],
+        ],
+      ]);
+    }
+  }
+
 }
