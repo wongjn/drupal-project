0162dd592d8a8ead166d6395c6a8347ddafcd0cd
wongjn <11310624+wongjn@users.noreply.github.com>
2019-02-18 15:17:27 +0000

    Remove loose from babel build

--- boilerplate/public_html/themes/custom/{{ NAME }}/babel.config.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
   api.cache.never();
 
   return {
-    presets: [['@babel/preset-env', { loose: true, modules: false }]],
+    presets: [['@babel/preset-env', { modules: false }]],
     plugins: [
       'lodash',
       '@babel/plugin-syntax-dynamic-import',
