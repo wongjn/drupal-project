342b070b511c703320778b556f49bfba2b80d211
wongjn <11310624+wongjn@users.noreply.github.com>
2019-09-06 21:55:20 +0100

    Remove static menu links from editable menus

---
+++ boilerplate/public_html/modules/custom/{{ NAME }}_helper/tests/src/Kernel/StaticMenuLinksAlterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\Tests\{{ NAME }}_helper\Kernel;
+
+use Drupal\Core\Menu\MenuTreeParameters;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests static menu links modifications.
+ *
+ * @group {{ NAME }}_helper
+ */
+class StaticMenuLinksAlterTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'system',
+    '{{ NAME }}_helper',
+    '{{ NAME }}_helper_test',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installConfig('system');
+
+    $this->container
+      ->get('plugin.manager.menu.link')
+      ->rebuild();
+  }
+
+  /**
+   * Tests static menu link removal on certain menus.
+   *
+   * @dataProvider linkRemovalMenuProvider
+   */
+  public function testLinkRemoval($menu_name, $removed = TRUE) {
+    $menu_tree = $this->container
+      ->get('menu.link_tree')
+      ->load($menu_name, new MenuTreeParameters());
+    $this->{$removed ? 'assertEmpty' : 'assertNotEmpty'}($menu_tree);
+  }
+
+  /**
+   * Provides menus to test static link removal.
+   *
+   * @return array
+   *   Menu IDs to test static link removal.
+   */
+  public function linkRemovalMenuProvider() {
+    return [
+      ['footer'],
+      ['main'],
+      ['admin', FALSE],
+    ];
+  }
+
+}
