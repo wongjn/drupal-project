ef957c545b37f7b047fe215677b56a00ff7ab67d
wongjn <11310624+wongjn@users.noreply.github.com>
2019-10-15 10:13:26 +0100

    Adapt Gulp SCSS incremental build for Sass modules

--- boilerplate/public_html/themes/custom/{{ NAME }}/gulpfile.js/css.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/gulpfile.js/css.js
@@ -5,11 +5,11 @@
 
 const { src, dest } = require('gulp');
 const cached = require('gulp-cached');
-const sassInheritance = require('gulp-sass-inheritance');
 const sass = require('gulp-sass');
 sass.compiler = require('sass');
 const postcss = require('gulp-postcss');
 const Fiber = require('fibers');
+const dependents = require('gulp-dependents');
 
 // SASS files glob.
 const sassSrc = './src/sass/**/*.scss';
@@ -25,7 +25,7 @@ function compileSass() {
 
   return src(sassSrc, { base, sourcemaps: true })
     .pipe(cached('sass'))
-    .pipe(sassInheritance({ dir: base }))
+    .pipe(dependents())
     .pipe(sass({ fiber: Fiber }).on('error', sass.logError))
     .pipe(postcss())
     .pipe(dest('dist/css', { sourcemaps: '.' }));
