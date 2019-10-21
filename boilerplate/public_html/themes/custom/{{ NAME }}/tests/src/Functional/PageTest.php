b66590cb7d2b45c5f69ad24b3cf539b271d8a103
wongjn <11310624+wongjn@users.noreply.github.com>
2019-10-21 16:16:48 +0100

    Favour functional tests for region tests

---
+++ boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Functional/PageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\{{ NAME }}\Functional;
+
+/**
+ * Tests page layout markup.
+ *
+ * @group {{ NAME }}
+ */
+class PageTest extends ThemeFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'block',
+  ];
+
+}
