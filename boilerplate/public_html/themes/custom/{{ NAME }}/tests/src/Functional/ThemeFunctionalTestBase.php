773620052ae00a2ea730e644db885e76abed0282
wongjn <11310624+wongjn@users.noreply.github.com>
2019-11-05 15:01:14 +0000

    Remove indentation on blank line

--- boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Functional/ThemeFunctionalTestBase.php
+++ boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Functional/ThemeFunctionalTestBase.php
@@ -9,7 +9,7 @@ use Drupal\Tests\{{ NAME }}\Traits\ThemeSetTrait;
  * A test base class for this theme.
  */
 abstract class ThemeFunctionalTestBase extends BrowserTestBase {
-  
+
   use ThemeSetTrait;
 
   /**
