974c7ba9aa1d993d6c530cd496d6cd0d73a2984c
wongjn <11310624+wongjn@users.noreply.github.com>
2020-04-03 09:19:30 +0000

    Clean index.js

--- boilerplate/public_html/themes/custom/{{ NAME }}/src/js/index.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/src/js/index.js
@@ -7,16 +7,25 @@ import './main-menu';
 import './scrollbar-size';
 import { behaviorsRegistrationInit } from './lib/behaviors';
 
-import(/* webpackChunkName: "async" */ './in-view').then(({ default: f }) =>
-  f(),
-);
+/**
+ * Invokes the default property of a given object.
+ *
+ * @param {Object} obj
+ *   The object given.
+ *
+ * @return {any}
+ *   Result from calling the `default` on the given object.
+ */
+const invokeDefault = ({ default: f }) => f();
+
+import(/* webpackChunkName: "async" */ './in-view').then(invokeDefault);
 
 if (BUNDLE_TYPE === 'legacy') {
   // All browsers that support ES modules natively also support external-use
   // SVGs, so only polyfill for the legacy bundle.
-  import(
-    /* webpackChunkName: "async" */ './lib/svg-polyfill'
-  ).then(({ default: f }) => f());
+  import(/* webpackChunkName: "async" */ './lib/svg-polyfill').then(
+    invokeDefault,
+  );
 }
 
 behaviorsRegistrationInit();
