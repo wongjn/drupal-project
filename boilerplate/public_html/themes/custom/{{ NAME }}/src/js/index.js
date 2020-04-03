c7902ba1e03e02d4d2a9658bdcd5fae9639e3263
wongjn <11310624+wongjn@users.noreply.github.com>
2020-04-03 09:17:59 +0000

    Fix later behaviors modules not registering

--- boilerplate/public_html/themes/custom/{{ NAME }}/src/js/index.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/src/js/index.js
@@ -5,7 +5,7 @@
 
 import './main-menu';
 import './scrollbar-size';
-import { lazyBehavior } from './lib/behaviors';
+import { behaviorsRegistrationInit } from './lib/behaviors';
 
 import(/* webpackChunkName: "async" */ './in-view').then(({ default: f }) =>
   f(),
@@ -19,34 +19,4 @@ if (BUNDLE_TYPE === 'legacy') {
   ).then(({ default: f }) => f());
 }
 
-/**
- * Register behaviors as lazy.
- *
- * @param {Object<string,string|null>} definitions
- *   Map of behaviors, key is the JS file to load in behaviors/ and value is
- *   the CSS selector or `null` for default value derived from the filename.
- *
- * @return {void}
- */
-const registerModules = definitions =>
-  Object.entries(definitions).forEach(([behavior, selector]) =>
-    // YAML cannot declare undefined value; use null instead.
-    lazyBehavior(behavior, selector === null ? undefined : selector),
-  );
-
-registerModules(drupalSettings.{{ CAMEL }}.modules);
-
-// Differential serving loads the legacy entry script asynchronously, meaning
-// that it may miss out on the DOMContentLoaded event on the document whereby
-// an initial Drupal.attachBehaviors() is called for the whole page. Hence
-// Drupal.attachBehaviors() is called again for the legacy bundle.
-if (BUNDLE_TYPE === 'legacy' && document.readyState !== 'loading') {
-  Drupal.attachBehaviors(document);
-}
-
-if (module.hot) {
-  module.hot.accept('./lib/behaviors', () => {
-    registerModules(drupalSettings.{{ CAMEL }}.modules);
-    Drupal.attachBehaviors(document.body, drupalSettings);
-  });
-}
+behaviorsRegistrationInit();
--- boilerplate/public_html/themes/custom/{{ NAME }}/src/js/lib/behaviors.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/src/js/lib/behaviors.js
@@ -3,6 +3,7 @@
  * Drupal behavior extension class to load extra JS asynchronously.
  */
 
+import { curry } from 'rambda';
 import { match } from './dom';
 
 // Noop function.
@@ -86,24 +87,35 @@ const capitalize = string =>
  * @param {string} selector
  *   Optional. CSS Selector to match relevant elements of this behavior. If not
  *   supplied, will default to `.js-${fileName}`.
+ * @param {boolean} [override=false]
+ *   Set to true to override existing behavior.
+ *
+ * @return {Drupal~behavior}
+ *   The created behavior.
  */
-export const lazyBehavior = (fileName, selector = `.js-${fileName}`) => {
+export const lazyBehavior = (
+  fileName,
+  selector = `.js-${fileName}`,
+  override = false,
+) => {
   const key = `{{ CAMEL }}${capitalize(fileName)}`;
 
-  Drupal.behaviors[key] = {
-    async attach(context, settings) {
-      if (match(selector, context).length === 0) {
-        return;
-      }
+  if (!(key in Drupal.behaviors) || override) {
+    Drupal.behaviors[key] = {
+      async attach(context, settings) {
+        if (match(selector, context).length === 0) {
+          return;
+        }
 
-      const { default: callback } = await import(
-        /* webpackChunkName: "behavior-[request]" */ `../behaviors/${fileName}`
-      );
+        const { default: callback } = await import(
+          /* webpackChunkName: "behavior-[request]" */ `../behaviors/${fileName}`
+        );
 
-      Drupal.behaviors[key] = behavior(selector, callback);
-      Drupal.behaviors[key].attach(context, settings);
-    },
-  };
+        Drupal.behaviors[key] = behavior(selector, callback);
+        Drupal.behaviors[key].attach(context, settings);
+      },
+    };
+  }
 
   if (module.hot) {
     module.hot.dispose(() => {
@@ -111,6 +123,61 @@ export const lazyBehavior = (fileName, selector = `.js-${fileName}`) => {
         Drupal.behaviors[key].detach(document.body, drupalSettings, 'unload');
       }
       delete Drupal.behaviors[key];
+    });
+  }
+
+  return Drupal.behaviors[key];
+};
+
+/**
+ * Runs attach on a behavior.
+ */
+const attach = curry((context, behavior0) => behavior0.attach(context));
+
+/**
+ * Register behaviors as lazy.
+ *
+ * @param {Object<string,string|null>} definitions
+ *   Map of behaviors, key is the JS file to load in behaviors/ and value is
+ *   the CSS selector or `null` for default value derived from the filename.
+ * @param {boolean} [override=false]
+ *   Set to true to override existing behaviors.
+ *
+ * @return {void}
+ */
+const registerModules = (definitions, override = false) =>
+  Object.entries(definitions).map(([behavior0, selector]) =>
+    // YAML cannot declare undefined value; use null instead.
+    lazyBehavior(behavior0, selector === null ? undefined : selector, override),
+  );
+
+/**
+ * Initializes async/lazy behavior registration.
+ */
+export const behaviorsRegistrationInit = () => {
+  /**
+   * Behavior to register lazy behavior modules.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.{{ CAMEL }}Modules = {
+    attach(context, settings) {
+      registerModules(settings?.{{ CAMEL }}?.modules || {}).forEach(attach(context));
+    },
+  };
+
+  // Differential serving loads the legacy entry script asynchronously, meaning
+  // that it may miss out on the DOMContentLoaded event on the document whereby
+  // an initial Drupal.attachBehaviors() is called for the whole page. Hence
+  // attach() is actually called for the first time here for the legacy bundle.
+  if (BUNDLE_TYPE === 'legacy' && document.readyState !== 'loading') {
+    Drupal.behaviors.{{ CAMEL }}Modules.attach(document);
+  }
+
+  if (module.hot) {
+    module.hot.accept(() => {
+      registerModules(drupalSettings.{{ CAMEL }}.modules, true);
+      Drupal.attachBehaviors(document.body, drupalSettings);
     });
   }
 };
