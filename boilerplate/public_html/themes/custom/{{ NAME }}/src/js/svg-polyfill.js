aff9f8d530eec9b21a6b39a87b69bc642eb0f48b
wongjn <11310624+wongjn@users.noreply.github.com>
2020-06-16 11:17:43 +0000

    Fix SVG polyfill not activating

--- boilerplate/public_html/themes/custom/{{ NAME }}/src/js/svg-polyfill.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/src/js/svg-polyfill.js
@@ -5,4 +5,5 @@
 
 import svg4everybody from 'svg4everybody';
 
-Drupal.behaviors.{{ CAMEL }}Svg4everybody = { attach: svg4everybody };
+Drupal.behaviors.{{ CAMEL }}Svg4everybody = { attach: () => svg4everybody() };
+Drupal.behaviors.{{ CAMEL }}Svg4everybody.attach();
