b33911e33b0b2b2d1a358bd988628067c3f4560e
wongjn <11310624+wongjn@users.noreply.github.com>
2020-07-14 08:40:39 +0000

    Update string list assertion functions

--- boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Traits/AssertOutputTrait.php
+++ boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Traits/AssertOutputTrait.php
@@ -27,25 +27,23 @@ trait AssertOutputTrait {
       $message ?: sprintf('%s has text "%s".', $css_selector, $text)
     );
   }
-  
+
   /**
    * Asserts a string is a space-separated list of values.
    *
    * Asserts values separated by spaces match but not necessarily in the same
    * order as expected.
    *
-   * @param string $expected
-   *   Expected value.
+   * @param string[] $expected
+   *   Expected values.
    * @param string $actual
    *   Actual value.
    * @param string $message
    *   (optional) Message for the test.
    */
-  protected function assertStringList($expected, $actual, $message = NULL) {
+  protected function assertStringList(array $expected, $actual, $message = NULL) {
     $actual_list = explode(' ', $actual);
-    $expected_list = explode(' ', $expected);
-
-    $this->assertEqualsCanonicalizing($expected_list, $actual_list, $message);
+    $this->assertEqualsCanonicalizing($expected, $actual_list, $message);
   }
 
   /**
@@ -54,18 +52,16 @@ trait AssertOutputTrait {
    * Asserts values separated by spaces match but not necessarily in the same
    * order as expected.
    *
-   * @param string $expected
-   *   Expected value.
+   * @param string[] $expected
+   *   Expected values.
    * @param string $actual
    *   Actual value.
    * @param string $message
    *   (optional) Message for the test.
    */
-  protected function assertStringListContains($expected, $actual, $message = NULL) {
+  protected function assertStringListContains(array $expected, $actual, $message = NULL) {
     $actual_list = explode(' ', $actual);
-    $expected_list = explode(' ', $expected);
-
-    $this->assertEmpty(array_diff($expected_list, $actual_list), $message);
+    $this->assertEmpty(array_diff($expected, $actual_list), $message);
   }
 
   /**
