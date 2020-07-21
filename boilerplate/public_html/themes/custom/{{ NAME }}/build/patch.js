4a8e079d56814eaa51932ff07856c06631af2033
wongjn <11310624+wongjn@users.noreply.github.com>
2020-07-21 13:46:03 +0000

    Upgrade svelte-preprocess dependency

--- boilerplate/public_html/themes/custom/{{ NAME }}/build/patch.js
+++ boilerplate/public_html/themes/custom/{{ NAME }}/build/patch.js
@@ -6,12 +6,6 @@ const { promises: fs } = require('fs');
 const path = require('path');
 
 [
-  // Remove once kaisermann/svelte-preprocess#149 lands.
-  {
-    file: 'svelte-preprocess/dist/transformers/scss.js',
-    search: 'y("node-sass","sass"',
-    replace: 'y("sass","node-sass"',
-  },
   // Remove once cssnano/cssnano#894 lands.
   {
     file: 'postcss-convert-values/dist/index.js',
