7fc02468efa1031878caee1fe40d9a6c2b977457
wongjn <11310624+wongjn@users.noreply.github.com>
2020-08-17 11:41:55 +0100

    Remove site_builder_console

--- public_html/sites/example.settings.local.php
+++ public_html/sites/example.settings.local.php
@@ -58,7 +58,6 @@ $settings['config_exclude_modules'][] = 'devel';
 $settings['config_exclude_modules'][] = 'devel_generate';
 $settings['config_exclude_modules'][] = 'field_ui';
 $settings['config_exclude_modules'][] = 'multiline_config';
-$settings['config_exclude_modules'][] = 'site_builder_console';
 $settings['config_exclude_modules'][] = 'views_ui';
 $settings['config_exclude_modules'][] = 'webprofiler';
 $settings['file_private_path'] = 'sites/default/private';
