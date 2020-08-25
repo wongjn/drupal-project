30f2519e6814a9513d18d52bf3f6a501509257bd
wongjn <11310624+wongjn@users.noreply.github.com>
2020-08-25 15:25:11 +0100

    Update page title block test

--- boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Kernel/Block/PageTitleBlockTest.php
+++ boilerplate/public_html/themes/custom/{{ NAME }}/tests/src/Kernel/Block/PageTitleBlockTest.php
@@ -1,40 +1,24 @@
 <?php
 
-namespace Drupal\Tests\{{ NAME }}\Functional\Block;
-
-use Drupal\Tests\BrowserTestBase;
+namespace Drupal\Tests\{{ NAME }}\Kernel\Block;
 
 /**
- * Tests output of the page title.
+ * Tests output of the page title block.
  *
  * @group {{ NAME }}
  */
-class PageTitleBlockTest extends BrowserTestBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  protected static $modules = ['block'];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected $defaultTheme = '{{ NAME }}';
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-    $this->drupalPlaceBlock('page_title_block', ['id' => 'title', 'region' => 'content']);
-  }
+class PageTitleBlockTest extends BlockTestBase {
 
   /**
    * Tests output.
    */
   public function testOutput() {
-    $this->drupalGet('');
-    $this->assertSession()->elementExists('css', '#block-title h1[class="o-title"]');
+    $block = $this->placeBlock('page_title_block', ['id' => 'block', 'region' => 'content']);
+    $block->getPlugin()->setTitle($this->randomMachineName());
+    $this->isolatedRender($this->viewBuilder->view($block));
+
+    $elements = $this->cssSelect('#block-block h1[class="o-title"]');
+    $this->assertCount(1, $elements);
   }
 
 }
